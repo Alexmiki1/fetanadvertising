72127d03f4c6dda721407fb89d4e0c8ed6e9dcb4
alex <alexxissmiki@gmail.com>
2026-09-21 12:36:07 -0500

    emailll fixed logoss change phone numberrRrsaa whats up number changeddss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Mono, Inter, League_Spartan } from "next/font/google";
+import Script from "next/script";
 import { siteMeta } from "@/lib/content";
 import { WhatsAppButton } from "@/components/WhatsAppButton";
 import "./globals.css";
@@ -67,7 +68,27 @@ export default function RootLayout({
       className={`${leagueSpartan.variable} ${inter.variable} ${ibmPlexMono.variable}`}
       style={{ colorScheme: "light" }}
     >
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GTM-MNZC2ZQQ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'GTM-MNZC2ZQQ');
+        `}
+      </Script>
       <body className={ibmPlexMono.className}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MNZC2ZQQ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <WhatsAppButton />
       </body>
